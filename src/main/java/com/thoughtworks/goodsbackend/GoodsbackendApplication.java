746355e95d3a8ec82f0c8055144b309b8353d651
chi.wang <wangchi327@163.com>
2018-09-17 11:43:34 +0800

    init

---
+++ src/main/java/com/thoughtworks/goodsbackend/GoodsbackendApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.goodsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsbackendApplication.class, args);
+    }
+}
